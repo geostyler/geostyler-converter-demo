0e4eb6bd89fa16bb430311fa01120bcc0b82cf22
Sylvain Lafay <sylvain.lafay@ign.fr>
2023-05-09 15:52:12 +0200

    chore(lint): replace doublequotes with singlequotes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import Editor from "@monaco-editor/react";
-import MapboxStyleParser from "geostyler-mapbox-parser";
-import SLDParser from "geostyler-sld-parser";
-import { useState } from "react";
-import logo from "./assets/images/logo.svg";
-import "./assets/styles/App.scss";
+import Editor from '@monaco-editor/react';
+import MapboxStyleParser from 'geostyler-mapbox-parser';
+import SLDParser from 'geostyler-sld-parser';
+import { useState } from 'react';
+import logo from './assets/images/logo.svg';
+import './assets/styles/App.scss';
 
 const getParser = (styleFormat: string) => {
     let parser = null;
     switch (styleFormat) {
-        case "mapbox":
+        case 'mapbox':
             parser = new MapboxStyleParser({ pretty: true });
             break;
-        case "sld":
+        case 'sld':
             parser = new SLDParser({ builderOptions: { format: true } });
             break;
         // case "qgis":
@@ -25,25 +25,25 @@ const getParser = (styleFormat: string) => {
 };
 
 function App() {
-    const [leftStyleFormat, setLeftStyleFormat] = useState("sld");
-    const [leftEditorText, setLeftEditorText] = useState("");
+    const [leftStyleFormat, setLeftStyleFormat] = useState('sld');
+    const [leftEditorText, setLeftEditorText] = useState('');
     const leftParser = getParser(leftStyleFormat);
 
-    const [rightStyleFormat, setRightStyleFormat] = useState("mapbox");
-    const [rightEditorText, setRightEditorText] = useState("");
+    const [rightStyleFormat, setRightStyleFormat] = useState('mapbox');
+    const [rightEditorText, setRightEditorText] = useState('');
     const rightParser = getParser(rightStyleFormat);
 
     const handleLeftEditorTextChange = (leftEditorText: string) => {
         leftParser
             ?.readStyle(leftEditorText)
             .then((gStyle) => {
-                if ("errors" in gStyle || gStyle?.errors?.length > 0) {
+                if ('errors' in gStyle || gStyle?.errors?.length > 0) {
                     console.error(gStyle.errors);
                 } else {
                     rightParser
                         ?.writeStyle(gStyle.output)
                         .then((rStyle) => {
-                            if ("errors" in rStyle || rStyle?.errors?.length > 0) {
+                            if ('errors' in rStyle || rStyle?.errors?.length > 0) {
                                 console.error(rStyle.errors);
                             } else {
                                 setRightEditorText(rStyle.output);
@@ -59,13 +59,13 @@ function App() {
         rightParser
             ?.readStyle(rightEditorText)
             .then((gStyle) => {
-                if ("errors" in gStyle || gStyle?.errors?.length > 0) {
+                if ('errors' in gStyle || gStyle?.errors?.length > 0) {
                     console.error(gStyle.errors);
                 } else {
                     leftParser
                         ?.writeStyle(gStyle.output)
                         .then((lStyle) => {
-                            if ("errors" in lStyle || lStyle?.errors?.length > 0) {
+                            if ('errors' in lStyle || lStyle?.errors?.length > 0) {
                                 console.error(lStyle.errors);
                             } else {
                                 setLeftEditorText(lStyle.output);
@@ -92,13 +92,13 @@ function App() {
             <div className="style-editor-container">
                 <Editor
                     height="90vh"
-                    defaultLanguage={leftStyleFormat === "mapbox" ? "json" : "xml"}
+                    defaultLanguage={leftStyleFormat === 'mapbox' ? 'json' : 'xml'}
                     value={leftEditorText}
                     onChange={handleLeftEditorTextChange}
                 />
                 <Editor
                     height="90vh"
-                    defaultLanguage={rightStyleFormat === "mapbox" ? "json" : "xml"}
+                    defaultLanguage={rightStyleFormat === 'mapbox' ? 'json' : 'xml'}
                     value={rightEditorText}
                     onChange={handleRightEditorTextChange}
                 />
